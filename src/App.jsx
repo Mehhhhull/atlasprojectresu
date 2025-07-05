9918ac4817c06f8d8ac1e2b42ee6e157962d02ec
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-07-05 21:31:35 +0530

    merge conflict solved

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to Atlas Project</h1>
+      <p className="paragraph">This is your React application</p>
+    </div>
+  )
+}
+
+export default App
